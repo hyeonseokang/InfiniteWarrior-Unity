f6fb70777b53ce0406fc20275e252edb661ceaee
hyunseo <hansx2079@naver.com>
2021-01-19 23:47:11 +0900

    GameManager 추가, 제네릭 Singleton 추가

---
+++ Assets/01_Scripts/00_Common/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public int coin
+    {
+        get;
+        private set;
+    }
+
+    private void Awake()
+    {
+        coin = 0;
+    }
+
+    public bool AddCoin(int addCoin)
+    {
+        if(coin + addCoin < 0)
+        {
+            Debug.LogError("코인이 마이너스 됨");
+            return true;
+        }
+
+        coin += addCoin;
+
+        return false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            AddCoin(100);
+        }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            AddCoin(-100);
+        }
+        else if(Input.GetKeyDown(KeyCode.F))
+        {
+            Debug.Log(coin);
+        }
+    }
+}
