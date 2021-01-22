8ccaa5c0025dacf2f1c62dd53a3f08a9bff76cda
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2021-01-22 22:35:27 +0900

    download

---
+++ Assets/01_Scripts/00_Common/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+
+                if (instance == null)
+                {
+                    GameObject singletonObject = new GameObject();
+                    instance = singletonObject.AddComponent<T>();
+                    DontDestroyOnLoad(instance);
+                }
+            }
+            return instance;
+        }
+    }
+}
