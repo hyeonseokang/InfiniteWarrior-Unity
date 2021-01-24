7a914a8a08a4f1067a317af387bdb80950dfce4d
hyunseo <hansx2079@naver.com>
2021-01-24 22:05:30 +0900

    SaveService 추가

---
+++ Assets/01_Scripts/00_Common/SaveService.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[System.Serializable]
+public class SaveData
+{
+    public int balance;
+    public int bestScore;
+    public int playCount;
+    public string character;
+
+    public static SaveData GetEmptyData()
+    {
+        SaveData data = new SaveData();
+        data.balance = 1000;
+        data.bestScore = 0;
+        data.playCount = 0;
+        data.character = "test1";
+
+        return data;
+    }
+}
+
+public class SaveService : Singleton<SaveService>
+{
+    private string dataPath;
+
+    private void Start()
+    {
+        dataPath = Application.persistentDataPath + "/dt.iw";
+    }
+
+    public void Save(SaveData data)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(dataPath);
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public SaveData Load()
+    {
+        if(File.Exists(dataPath))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(dataPath, FileMode.Open);
+            SaveData data = (SaveData)bf.Deserialize(file);
+            file.Close();
+
+            return data;
+        }
+
+        return SaveData.GetEmptyData();
+    }
+}
---
+++ Assets/01_Scripts/Character.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    protected int hp;
+    public virtual void Attack()
+    {
+
+    }
+}
