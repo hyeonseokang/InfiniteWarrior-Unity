f0ab0e498287fb5cb2d255d9955ac4c58213d6fe
hyunseo <hansx2079@naver.com>
2021-01-24 21:38:59 +0900

    SoundManager 추가

---
+++ Assets/01_Scripts/00_Common/PlayerInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : Singleton<PlayerInfo>
+{
+    public int balance { get; set; }
+    public int bestScore { get; set; }
+    public int playCount { get; set; }
+}
---
+++ Assets/01_Scripts/01_MainMenu/MainMenuService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class MainMenuService : MonoBehaviour
+{
+    public Button clickButton;
+    private void Start()
+    {
+        SoundManager.Instance.PlayBGM();
+        clickButton.onClick.AddListener(() =>
+        {
+            SoundManager.Instance.PlaySFX(SFX.ButtonClick);
+        });
+    }
+}
