564452e611767112858910f2d884c9524e5151aa
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2021-01-25 22:35:14 +0900

    merge
    진행상황 결합

---
+++ Assets/01_Scripts/00_Common/item.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ItemState { none, purchased,player};
+public enum ButtonState { buy, select , selected};
+public class item : MonoBehaviour
+{
+    public shop shoppingManager;
+
+    public int id;
+    public string name;
+    public int price;
+    public int speed;
+    public ItemState state = ItemState.none;
+
+    private Outline outline;
+
+    public void Awake()
+    {
+        outline = GetComponent<Outline>();
+        outline.enabled = false;
+    }
+    public void setOutLine(bool value)
+    {
+        outline.enabled = value;
+    }
+  
+    public void changeState(ItemState s)
+    {
+        state = s;
+    }
+    public void OnClickItem()
+    {
+        shoppingManager.updateSelectedItem(id);
+
+    }
+
+    
+
+}
