872f737e0680ee759e91b17e6d443e8e62be79cc
hyunseo <hansx2079@naver.com>
2021-01-26 21:01:41 +0900

    프로젝트 다시 세팅

---
+++ Assets/01_Scripts/00_Common/TestSoundManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSoundManager : MonoBehaviour
+{
+    private void Start()
+    {
+        SoundManager.Instance.PlayBGM();
+    }
+    public void PlaySound()
+    {
+        SoundManager.Instance.PlaySFX(SFX.ButtonClick);
+    }
+}
