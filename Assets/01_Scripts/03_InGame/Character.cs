7786176505528e84ea8ab704fcf8caf854e3270c
hyunseo <hansx2079@naver.com>
2021-02-07 19:41:55 +0900

    Factory 추가

--- Assets/01_Scripts/03_InGame/Character.cs
+++ Assets/01_Scripts/03_InGame/Character.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Character : MonoBehaviour
+public class Character : MonoBehaviour
 {
     protected int hp;
-    public virtual void Attack()
+    private float attackDistance = 5.0f;
+
+    public  void Attack()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, attackDistance);
+        Debug.DrawRay(transform.position, transform.right * attackDistance, Color.red, 0.3f);
+
+        if (hit)
+        {
+            if (hit.collider.CompareTag("monster"))
+            {
+                Monster monster = hit.collider.GetComponent<Monster>();
+                monster.Hit();
+            }
+        }
+    }
+
+    public  void Move()
     {
 
     }
---
+++ Assets/01_Scripts/03_InGame/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMonster
+{
+    public void Hit();
+}
+
+public abstract class Monster : MonoBehaviour, IMonster
+{
+    public void Hit()
+    {
+
+    }
+}
