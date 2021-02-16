244b78411af4f9d95fb3d8f299a11cbe1ade590c
hyunseo <hansx2079@naver.com>
2021-02-16 17:44:32 +0900

    Monster 일부 수정

--- Assets/01_Scripts/03_InGame/Monster.cs
+++ Assets/01_Scripts/03_InGame/Monster.cs
@@ -9,9 +9,22 @@ public interface IMonster
 
 public abstract class Monster : MonoBehaviour, IMonster
 {
+    public float attackDistance = 5.0f;
+    public void Attack()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, attackDistance);
+        
+        if (hit)
+        {
+            if (hit.collider.CompareTag("character"))
+            {
+                InGameEventService.Instance.hitCharacterEvent();
+            }
+        }
+    }
     public void Hit()
     {
-
+        Destroy(gameObject);
     }
 
     public static IMonster CheckMonster(GameObject checkObject)
