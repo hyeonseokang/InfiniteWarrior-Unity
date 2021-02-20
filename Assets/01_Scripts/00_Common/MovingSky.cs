322d8eafa1e970c061753cacdf07f58049759789
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2021-02-20 18:00:56 +0900

    sky anim

---
+++ Assets/01_Scripts/00_Common/MovingSky.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingSky : MonoBehaviour
+{
+
+    public float ScrollSpeed = 3f;
+    float Offset;
+    Renderer renderer;
+     
+     void Start(){
+         renderer=GetComponent<Renderer>();
+     }
+    void Update ()
+    {      
+        Offset += Time.deltaTime * ScrollSpeed;
+        renderer.material.mainTextureOffset = new Vector2 (Offset, 0);     
+    }
+
+}
