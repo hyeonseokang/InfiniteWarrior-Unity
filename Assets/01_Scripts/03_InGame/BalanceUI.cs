0e67d2e3c0f61d0ecf2408215dd85c96d394c4ac
hyunseo <hansx2079@naver.com>
2021-03-01 13:40:11 +0900

    InGame Balance 표시

---
+++ Assets/01_Scripts/03_InGame/BalanceUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BalanceUI : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI balacneText;
+
+    private void Start()
+    {
+        SetBalacneText(PlayerInfo.Instance.GetBalance().ToString());    
+    }
+
+    public void SetBalacneText(string text)
+    {
+        balacneText.SetText(text);
+    }
+}
